c9c9f85f78c18aff882ad09d1b7c978b14613c65
cardona.sj@gmail.com <cardona.sj@gmail.com>
2019-04-11 08:17:50 -0500

    initial commit / fix csv to list issue

---
+++ SqlDDLWriter.py
@@ -0,0 +1,21 @@
+import csv
+
+# filehandler = open("CustomerCallCenterDDl.sql", "w")
+
+dataDictionary = open("/Users/steven/Desktop/fruitlist.csv", "r")
+finalDataDict = csv.reader(dataDictionary, delimiter=',')
+for row in finalDataDict:
+    list1 = row
+    tableName = list1[0]
+    sqlType = list1[1]
+    isNull = list1[2]
+    isPrimaryKey = list1[3]
+    autoIncrement = list1[4]
+    comment = list1[4]
+    print(tableName, ", ", sqlType, ", ", isNull, ", ", isPrimaryKey, ", ", autoIncrement, ", ", comment)
+# sqlfile = open("Customer_Call_Center_DDL.sql", "a")
+
+print("\nDROP DATABASE IF EXISTS call_center"
+      "\nCREATE DATABASE  call_center")
+
+# for row in finalDataDict:
