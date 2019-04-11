9d319b937a58ae8c29b23e7eb7dd34548c85aeff
Steven Cardona <cardona.sj@gmail.com>
2019-04-11 09:43:34 -0500

    Get create tables working

--- SqlDDLWriter.py
+++ SqlDDLWriter.py
@@ -1,21 +1,45 @@
 import csv
 
+# sqlfile = open("Customer_Call_Center_DDL.sql", "a")
 # filehandler = open("CustomerCallCenterDDl.sql", "w")
-
+print("\nDROP DATABASE IF EXISTS call_center"
+      "\nCREATE DATABASE  call_center")
 dataDictionary = open("fruitlist.csv", "r")
 finalDataDict = csv.reader(dataDictionary, delimiter=',')
+tableNameCheck = ""
+tableList = []
+
 for row in finalDataDict:
     list1 = row
     tableName = list1[0]
+    fieldName = list1[1]
     sqlType = list1[1]
     isNull = list1[2]
     isPrimaryKey = list1[3]
     autoIncrement = list1[4]
     comment = list1[4]
-    print(tableName, ", ", sqlType, ", ", isNull, ", ", isPrimaryKey, ", ", autoIncrement, ", ", comment)
-# sqlfile = open("Customer_Call_Center_DDL.sql", "a")
+    if tableNameCheck != tableName:
+        tableList.append(tableName)
+        print("}")
+        print()
+        print("CREATE TABLE ", tableName, " {")
+    print("\t", fieldName, ", ", sqlType, ", ", isNull, ", ", isPrimaryKey, ", ", autoIncrement, ", ", comment)
+    tableNameCheck = tableName
 
-print("\nDROP DATABASE IF EXISTS call_center"
-      "\nCREATE DATABASE  call_center")
+dataDictionary.close()
 
-# for row in finalDataDict:
+# TODO: Get Insert statements working
+#
+# exampleData = open("fruitlist.csv", "r")
+# data = csv.reader(exampleData, delimiter=",")
+# dataList = list(data)
+# index = 0
+# # dataFormat = "(" + data + ")"
+# for table in tableList:
+#     print("INSERT INTO ", table, "VALUES")
+#     if index == len(list(data)):
+#         for data1 in dataList:
+#             dataFormat = "(" + data1[index] + ")"
+#             print(dataFormat).strip('\n')
+#             index += 1
+#
