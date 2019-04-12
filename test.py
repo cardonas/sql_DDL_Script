ecfd3757296bea52534f0d197a48fdaf9769ea7e
Steven Cardona <cardona.sj@gmail.com>
2019-04-12 17:13:42 -0500

    fix csv file formating

--- test.py
+++ test.py
@@ -14,4 +14,17 @@ tableNameList = []
 for col in finalDataDict:
     if col[0] != '':
         tableNameList.append(col[0])
-print(tableNameList)
+dataDictionary.close()
+
+dataDictionary = open("tablelist.csv", "r")
+finalDataDict = csv.reader(dataDictionary, delimiter=',')
+for row in finalDataDict:
+    field1 = row[1]
+    field2 = row[2]
+    field3 = row[3]
+    field4 = row[4]
+    field5 = row[5]
+
+    print(field1, field5, field3, field4, field5)
+
+
